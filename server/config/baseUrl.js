4711991fc6224c9e568a005a18c3db032acfdbcf
chengtingsen <chengtingsen@163.com>
2018-05-12 19:56:54 +0800

    修改配置

--- server/config/baseUrl.js
+++ server/config/baseUrl.js
@@ -3,7 +3,7 @@ const base = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  base.reqUrl = "http://api.qiluyidian.mobi:8081"
+  base.reqUrl = "http://222.175.121.249:8081"
 } else {
   base.reqUrl = "http://222.175.121.249:8081"
 }
